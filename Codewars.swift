59a2eadcfab607f8e65eafef95b6faf70250cc46
KAREN VARDANIAN <103530402+KarVarr@users.noreply.github.com>
2022-12-30 23:07:15 +0300

    added Count the Digit and 6kyu Persistent Bugger.

--- Codewars.swift
+++ Codewars.swift
@@ -52,10 +52,47 @@ func inviteMoreWomen(_ arr: [Int]) -> Bool {
 Drying Potatoes
 */
 
+func nbDig(_ n: Int, _ d: Int) -> Int {
+   (0...n).map{ "\($0 * $0)".filter {$0 == Character("\(d)") } }.flatMap{$0}.count
+}
+
+//---------------------------------------------------
+
+/*
+Count the Digit
+*/
+
 func potatoes (_ p0: Int, _ w0: Int, _ p1: Int) -> Int {
    w0 * (100 - p0) / (100 - p1) 
 }
 
 //---------------------------------------------------
 
+
+
+
+
+
+
+//MARK: - 6kyu
+
+/*
+Persistent Bugger.
+*/
+
+func persistence(for num: Int) -> Int {
+  var arr = String(num).compactMap{$0.wholeNumberValue}.reduce(1, *)
+  var count = 1
+    if arr < 8 {
+      return 0
+    } else {
+        while arr >= 9  {
+            arr = String(arr).compactMap{$0.wholeNumberValue}.reduce(1, *)
+            count += 1
+      }
+    }
+   return count
+}
+
+//---------------------------------------------------
 
