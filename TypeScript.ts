dd8d1683284dea89d8063fa586f004e6cff40488
Karen Vardanian <103530402+KarVarr@users.noreply.github.com>
2023-01-21 09:45:18 +0300

    Create TypeScript.ts

---
+++ TypeScript.ts
@@ -0,0 +1,13 @@
+My solutions TypeScript
+
+
+//8 kyu
+
+/*
+Even or Odd
+*/
+export function even_or_odd(n:number):string {
+  return n % 2 == 0 ? "Even" : "Odd"
+}
+
+//---------------------------------------------------
