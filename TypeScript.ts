3fa0cf3d95bf2cef1a83e9aea40f880fbce2e1d0
Karen Vardanian <103530402+KarVarr@users.noreply.github.com>
2023-02-03 10:45:24 +0300

    added Keep up the hoop

--- TypeScript.ts
+++ TypeScript.ts
@@ -271,6 +271,15 @@ export const addFive = (num : number) : number => {
 
 //---------------------------------------------------
 
+/*
+Keep up the hoop
+*/
+
+export function hoopCount(n: number): string {
+  return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it"
+}
+
+//---------------------------------------------------
 
 
 //7 kyu
