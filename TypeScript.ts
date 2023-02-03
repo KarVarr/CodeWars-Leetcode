d11e7fa974472a7d900edce3c3aca22df0d22483
Karen Vardanian <103530402+KarVarr@users.noreply.github.com>
2023-02-03 10:56:15 +0300

    added Opposites Attract

--- TypeScript.ts
+++ TypeScript.ts
@@ -281,8 +281,22 @@ export function hoopCount(n: number): string {
 
 //---------------------------------------------------
 
+/*
+Opposites Attract
+*/
 
-//7 kyu
+export function lovefunc(flower1: number, flower2: number): boolean {
+    return (flower1 + flower2) % 2 == 1 ? true : false 
+}
+
+//---------------------------------------------------
+
+
+
+
+
+
+//7 kyu ============================================
 
 /*
 Reverse words
