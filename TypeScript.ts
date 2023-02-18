9dfa6f9faba2ceb31a7170d2f752ce2528a14b26
Karen Vardanian <103530402+KarVarr@users.noreply.github.com>
2023-02-18 09:35:56 +0300

    added N-th Power

--- TypeScript.ts
+++ TypeScript.ts
@@ -628,6 +628,15 @@ export const distinct = (a: number[]): number[] => {
 
 //---------------------------------------------------
 
+/*
+N-th Power
+*/
+
+export function index(array: number[], n: number): number {
+  return array.length <= n ? -1 : array[n]**n;
+}
+
+//---------------------------------------------------
 
 
 
