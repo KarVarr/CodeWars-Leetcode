297bc26c4bc64ff0fb96a6eaa4b55ef96c8aa259
Karen Vardanian <103530402+KarVarr@users.noreply.github.com>
2023-02-22 08:41:07 +0300

    added Beginner Series #1 School Paperwork

--- TypeScript.ts
+++ TypeScript.ts
@@ -688,6 +688,15 @@ export function goals (laLigaGoals:number, copaDelReyGoals:number, championsLeag
 
 //---------------------------------------------------
 
+/*
+Beginner Series #1 School Paperwork
+*/
+
+export function paperwork(n: number, m :number): number{
+  return n < 0 || m < 0 ? 0 : n * m;
+}
+
+//---------------------------------------------------
 
 
 
