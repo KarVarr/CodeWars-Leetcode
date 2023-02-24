cc486127d39850383296cb3502cdf7afa09c3535
Karen Vardanian <103530402+KarVarr@users.noreply.github.com>
2023-02-24 11:59:32 +0300

    added Rock Paper Scissors!

--- TypeScript.ts
+++ TypeScript.ts
@@ -791,6 +791,20 @@ export function position(alphabet: string): string {
 
 //---------------------------------------------------
 
+/*
+Rock Paper Scissors!
+*/
+
+export function rps(p1: string, p2: string): string{
+  if (p1 == "scissors" && p2 == "paper" || p1 == "rock" && p2 == "scissors" || p1 == "paper" && p2 == "rock") {
+    return "Player 1 won!"
+  } else if (p1 === p2 ) {
+    return 'Draw!'
+  }
+  return "Player 2 won!"
+}
+
+//---------------------------------------------------
 
 
 
