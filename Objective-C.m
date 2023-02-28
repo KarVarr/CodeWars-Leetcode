eed07476ae4ce276429a282c377eabdf3397dbc9
Karen Vardanian <103530402+KarVarr@users.noreply.github.com>
2023-02-28 13:29:10 +0300

    added Is n divisible by x and y?

--- Objective-C.m
+++ Objective-C.m
@@ -55,4 +55,24 @@ int century(int year) {
 
 //---------------------------------------------------
 
+/*
+Is n divisible by x and y?
+
+*/
+
+int isDivisible(int n, int x, int y) {
+  if (n % x == 0 && n % y == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//---------------------------------------------------
+
+
+
+
+
+
 
