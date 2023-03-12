f682859c7d465346ace9babdf1ed9c166eef40eb
Karen Vardanian <103530402+KarVarr@users.noreply.github.com>
2023-03-12 11:38:33 +0300

    added 509. Fibonacci Number

--- LeetcodeSwift.swift
+++ LeetcodeSwift.swift
@@ -262,5 +262,18 @@ class Solution {
 
 //---------------------------------------------------
 
+/*
+509. Fibonacci Number
+*/
+
+class Solution {
+    func fib(_ n: Int) -> Int {
+        if n == 0 {return 0}
+        if n == 1 {return 1}
+        return fib(n - 1) + fib(n - 2)
+    }
+}
+
+//---------------------------------------------------
 
 
