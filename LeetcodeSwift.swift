b245e264ef41519ad067db7a6c04ea8847b7ad2f
Karen Vardanian <103530402+KarVarr@users.noreply.github.com>
2023-03-28 13:32:34 +0300

    added 1512. Number of Good Pairs

--- LeetcodeSwift.swift
+++ LeetcodeSwift.swift
@@ -515,6 +515,25 @@ class Solution {
 
 //---------------------------------------------------
 
+/*
+1512. Number of Good Pairs
+*/
+
+class Solution {
+    func numIdenticalPairs(_ nums: [Int]) -> Int {
+        var count = 0
+        for i in 0..<nums.count {
+            for j in i+1..<nums.count {
+                if nums[i] == nums[j] {
+                    count += 1
+                }
+            }
+        }
+        return count
+    }
+}
+
+//---------------------------------------------------
 
 
 
