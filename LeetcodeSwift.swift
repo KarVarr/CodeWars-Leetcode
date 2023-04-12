c65afa1fe0be82537032bf795f2aa5b6abb41ea7
Karen Vardanian <103530402+KarVarr@users.noreply.github.com>
2023-04-12 10:20:00 +0300

    added 2427. Number of Common Factors

--- LeetcodeSwift.swift
+++ LeetcodeSwift.swift
@@ -771,6 +771,23 @@ class Solution {
 
 //---------------------------------------------------
 
+/*
+2427. Number of Common Factors
+*/
+
+class Solution {
+    func commonFactors(_ a: Int, _ b: Int) -> Int {
+    var count = 0
+    for i in 1...min(a,b) {
+        if a % i == 0 && b % i == 0 {
+            count += 1
+        }
+    }
+    return count
+    }
+}
+
+//---------------------------------------------------
 
 
 
