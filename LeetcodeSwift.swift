1d2b73a5c5ccad03ffd63f2af3a0078973eadf79
Karen Vardanian <103530402+KarVarr@users.noreply.github.com>
2023-04-14 10:35:37 +0300

    added 1662. Check If Two String Arrays are Equival

--- LeetcodeSwift.swift
+++ LeetcodeSwift.swift
@@ -814,6 +814,17 @@ class Solution {
 
 //---------------------------------------------------
 
+/*
+1662. Check If Two String Arrays are Equivalent
+*/
+
+class Solution {
+    func arrayStringsAreEqual(_ word1: [String], _ word2: [String]) -> Bool {
+        word1.reduce(" ", +) == word2.reduce(" ", +) ? true : false
+    }
+}
+
+//---------------------------------------------------
 
 
 
