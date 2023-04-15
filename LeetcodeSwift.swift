0ed493cd8ba50cb87abcafb76206ea4bfdc77930
Karen Vardanian <103530402+KarVarr@users.noreply.github.com>
2023-04-15 11:43:30 +0300

    added 938. Range Sum of BST

--- LeetcodeSwift.swift
+++ LeetcodeSwift.swift
@@ -826,6 +826,47 @@ class Solution {
 
 //---------------------------------------------------
 
+/*
+1662. Check If Two String Arrays are Equivalent
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func rangeSumBST(_ root: TreeNode?, _ low: Int, _ high: Int) -> Int {
+       guard let root = root else { return 0 }
+
+       var sum = 0
+       if root.val >= low && root.val <= high {
+           sum += root.val
+       }
+
+       if root.val > low {
+           sum += rangeSumBST(root.left, low, high)
+       }
+
+       if root.val < high {
+           sum += rangeSumBST(root.right, low, high)
+       }
+       
+       return sum
+    }
+}
+
+//---------------------------------------------------
 
 
 
