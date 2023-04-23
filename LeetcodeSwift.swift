5be5b700effb843dd3e0ac8bc7aa6e877ec64854
Karen Vardanian <103530402+KarVarr@users.noreply.github.com>
2023-04-23 11:47:51 +0300

    added 28. Find the Index of the First Occurrence

--- LeetcodeSwift.swift
+++ LeetcodeSwift.swift
@@ -1018,6 +1018,18 @@ class Solution {
 
 //---------------------------------------------------
 
+/*
+28. Find the Index of the First Occurrence in a String
+*/
+
+class Solution {
+    func strStr(_ haystack: String, _ needle: String) -> Int {
+        guard let range = haystack.range(of: needle) else { return -1 }
+        return haystack.distance(from: haystack.startIndex, to: range.lowerBound)
+    }
+}
+
+//---------------------------------------------------
 
 
 
