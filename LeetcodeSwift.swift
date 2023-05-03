8c6ae4d8c10eb817e6728c2ba1326bddbc7cebaf
Karen Vardanian <103530402+KarVarr@users.noreply.github.com>
2023-05-03 08:29:13 +0300

    added 2215. Find the Difference of Two Arrays

--- LeetcodeSwift.swift
+++ LeetcodeSwift.swift
@@ -1165,3 +1165,26 @@ class Solution {
 }
 
 //---------------------------------------------------
+
+/*
+2215. Find the Difference of Two Arrays
+*/
+
+class Solution {
+    func findDifference(_ nums1: [Int], _ nums2: [Int]) -> [[Int]] {
+            let set1 = Set(nums1)
+            let set2 = Set(nums2)
+
+            let diff1 = set1.subtracting(set2)
+            let diff2 = set2.subtracting(set1)
+
+            let diff1Array = Array(diff1)
+            let diff2Array = Array(diff2)
+
+            let output = [diff1Array, diff2Array]
+            
+        return output
+    }
+}
+
+//---------------------------------------------------
