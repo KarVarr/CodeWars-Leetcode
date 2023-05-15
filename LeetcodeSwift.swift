3e476bbb93e88757a8432afca28e7dbffc61a9b5
Karen Vardanian <103530402+KarVarr@users.noreply.github.com>
2023-05-15 13:40:57 +0300

    Update LeetcodeSwift.swift
    206. Reverse Linked List

--- LeetcodeSwift.swift
+++ LeetcodeSwift.swift
@@ -1249,3 +1249,39 @@ class Solution {
 }
 
 //---------------------------------------------------
+
+/*
+1812. Determine Color of a Chessboard Square
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        if head == nil {
+            return head
+        }
+
+        var prev: ListNode? = nil
+        var curr = head
+
+        while curr != nil {
+            let next = curr?.next
+            curr?.next = prev
+            prev = curr
+            curr = next
+        }
+
+        return prev
+    }
+}
+
+//---------------------------------------------------
