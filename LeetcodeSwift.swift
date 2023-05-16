d904c1eafcacf055023bf6fa4c91c030ab7b8c9c
Karen Vardanian <103530402+KarVarr@users.noreply.github.com>
2023-05-16 10:46:57 +0300

    Update LeetcodeSwift.swift
    24. Swap Nodes in Pairs

--- LeetcodeSwift.swift
+++ LeetcodeSwift.swift
@@ -1285,3 +1285,31 @@ class Solution {
 }
 
 //---------------------------------------------------
+
+/*
+24. Swap Nodes in Pairs
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func swapPairs(_ head: ListNode?) -> ListNode? {
+    guard let head = head, let next = head.next else { return head }
+    
+    let newHead = next
+    head.next = swapPairs(next.next)
+    newHead.next = head
+    
+    return newHead
+    }
+}
+
+//---------------------------------------------------
