10f00b43575def9cc9efb33ec1018640c772f36c
Karen Vardanian <103530402+KarVarr@users.noreply.github.com>
2023-05-24 14:36:28 +0300

    Update CodeWarsSwift.swift
    func toNato(_ words: String) -> String {
      var result = [String]()
      
      let str = Array(words).map{$0.uppercased()}
      
      let nato: [Character: String] = [
        "A": "Alfa ", "B": "Bravo ", "C": "Charlie ", "D": "Delta ", "E": "Echo ",
        "F": "Foxtrot ", "G": "Golf ", "H": "Hotel ", "I": "India ", "J": "Juliett ",
        "K": "Kilo ", "L": "Lima ", "M": "Mike ", "N": "November ", "O": "Oscar ",
        "P": "Papa ", "Q": "Quebec ", "R": "Romeo ", "S": "Sierra ", "T": "Tango ",
        "U": "Uniform ", "V": "Victor ", "W": "Whiskey ", "X": "Xray ", "Y": "Yankee ",
        "Z": "Zulu ", "0": "Zero ", "1": "One ", "2": "Two ", "3": "Three ", "4": "Four ",
        "5": "Five ", "6": "Six ", "7": "Seven ", "8": "Eight ", "9": "Nine ",
        ",": ",", ".": ".", "?": "?", "!": "!", "(": "( ", ")": ") ", "\"": "\" "
    ]
       
       for word in str {
         result.append(nato[Character(word)] ?? "")
       }
      
      return result.map{String($0)}.joined(separator: "").trimmingCharacters(in: .whitespacesAndNewlines)
    }

--- CodeWarsSwift.swift
+++ CodeWarsSwift.swift
@@ -1007,6 +1007,34 @@ func isAValidMessage(_ message: String) -> Bool {
 
 //---------------------------------------------------
 
+/*
+If you can read this...
+*/
+
+func toNato(_ words: String) -> String {
+  var result = [String]()
+  
+  let str = Array(words).map{$0.uppercased()}
+  
+  let nato: [Character: String] = [
+    "A": "Alfa ", "B": "Bravo ", "C": "Charlie ", "D": "Delta ", "E": "Echo ",
+    "F": "Foxtrot ", "G": "Golf ", "H": "Hotel ", "I": "India ", "J": "Juliett ",
+    "K": "Kilo ", "L": "Lima ", "M": "Mike ", "N": "November ", "O": "Oscar ",
+    "P": "Papa ", "Q": "Quebec ", "R": "Romeo ", "S": "Sierra ", "T": "Tango ",
+    "U": "Uniform ", "V": "Victor ", "W": "Whiskey ", "X": "Xray ", "Y": "Yankee ",
+    "Z": "Zulu ", "0": "Zero ", "1": "One ", "2": "Two ", "3": "Three ", "4": "Four ",
+    "5": "Five ", "6": "Six ", "7": "Seven ", "8": "Eight ", "9": "Nine ",
+    ",": ",", ".": ".", "?": "?", "!": "!", "(": "( ", ")": ") ", "\"": "\" "
+]
+   
+   for word in str {
+     result.append(nato[Character(word)] ?? "")
+   }
+  
+  return result.map{String($0)}.joined(separator: "").trimmingCharacters(in: .whitespacesAndNewlines)
+}
+
+//---------------------------------------------------
 
 
 
