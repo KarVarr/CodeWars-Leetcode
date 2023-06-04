b1ffa1c759e1811a8fefc3623fbe6c7758e39c3d
Karen Vardanian <103530402+KarVarr@users.noreply.github.com>
2023-06-04 10:59:43 +0300

    Update LeetcodeSwift.swift
    263. Ugly Number

--- LeetcodeSwift.swift
+++ LeetcodeSwift.swift
@@ -1417,3 +1417,28 @@ class Solution {
 }
 
 //---------------------------------------------------
+
+/*
+263. Ugly Number
+*/
+
+class Solution {
+    func isUgly(_ n: Int) -> Bool {
+        guard n > 0 else { return false }
+
+        var num = n
+        while num % 2 == 0 {
+            num /= 2
+        }
+        while num % 3 == 0 {
+            num /= 3
+        }
+        while num % 5 == 0 {
+            num /= 5
+        }
+
+        return num == 1
+    }
+}
+
+//---------------------------------------------------
